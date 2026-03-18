7014677219082db29173aceed130a963f17040d5
gracexu-crypto <grace_xu@brown.edu>
2026-03-18 11:22:21 +0800

    Writed by Grace, Time spent writing: 1.5 hours; Debugged by Grace, Time spent debugging: 1.5 hours.

--- blackjack.m
+++ blackjack.m
@@ -1,3 +1,47 @@
+clc;
+clear;
+
+
+disp('Welcome to Blackjack!')
+
+% Create and shuffle deck
+myDeck = createDeck();
+myShuffledDeck = shuffleDeck(myDeck);
+
+% Deal initial cards
+[playerHand, dealerHand, nextCard] = dealCards(myShuffledDeck);
+
+% Show initial hands
+disp('Your starting hand:')
+displayHand(playerHand);
+fprintf('Your hand value: %d\n\n', calculateHandValue(playerHand));
+
+
+fprintf('\n');
+
+% Player turn
+[playerHand, nextCard, playerBust, playerStand] = playerTurn(myShuffledDeck, nextCard, playerHand);
+
+
+if playerBust
+    disp('You busted. Dealer wins.')
+else
+    [dealerHand, nextCard] = dealerTurn(myShuffledDeck, nextCard, dealerHand);
+
+    % Show final hands
+    disp('Final Player Hand:')
+    displayHand(playerHand);
+    fprintf('Player hand value: %d\n\n', calculateHandValue(playerHand));
+
+    disp('Final Dealer Hand:')
+    displayHand(dealerHand);
+    fprintf('Dealer hand value: %d\n\n', calculateHandValue(dealerHand));
+
+    % Determine winner
+    result = determineWinner(playerHand, dealerHand);
+    disp(result)
+end
+
 function deck = createDeck()
     % Create a standard 52-card deck matrix (4 suits, values 1-13)
     % 1 = Ace, 11 = Jack, 12 = Queen, 13 = King
@@ -12,53 +56,33 @@ function shuffledDeck = shuffleDeck(deck)
     shuffledDeck = deck(randperm(num_cards));
 end
 
-% test the function
-myDeck = createDeck();
-myShuffledDeck = shuffleDeck(myDeck);
-disp(myShuffledDeck);
-
 function [playerHand, dealerHand, nextCard] = dealCards(shuffledDeck)
     % Deal two cards each to player and dealer
     playerHand = [shuffledDeck(1) shuffledDeck(3)];
     dealerHand = [shuffledDeck(2) shuffledDeck(4)];
 
-% track the next card position
+    % Track the next card position
     nextCard = 5;
 end
 
 function [hand, nextCard] = hitCard(shuffledDeck, nextCard, hand)
-% deal one additional card
+    % Deal one additional card
     hand(end+1) = shuffledDeck(nextCard);
     nextCard = nextCard + 1;
 end
 
-[playerHand, dealerHand, nextCard] = dealCards(myShuffledDeck);
-
-disp('Player Hand:')
-disp(playerHand)
-
-disp('Dealer Hand:')
-disp(dealerHand)
-
-[playerHand, nextCard] = hitCard(myShuffledDeck, nextCard, playerHand);
-
-disp('Player Hand After Hit:')
-disp(playerHand)
-
-% (Graces Part)
-
 function handValue = calculateHandValue(hand)
-    % convert face cards (11,12,13) to 10
+    % Convert face cards (11,12,13) to 10
     values = hand;
     values(values > 10) = 10;
 
-    % count aces
+    % Count aces
     numAces = sum(values == 1);
 
-    % start by treating all aces as 11
-    handValue = sum (values(values ~= 1)) + numAces * 11;
+    % Start by treating all aces as 11
+    handValue = sum(values(values ~= 1)) + numAces * 11;
 
-    % adjust aces from 11 to 1 if bust
+    % Adjust aces from 11 to 1 if bust
     while handValue > 21 && numAces > 0
         handValue = handValue - 10;
         numAces = numAces - 1;
@@ -66,6 +90,87 @@ function handValue = calculateHandValue(hand)
 end
 
 function bust = isBust(hand)
-    % Returns true if hand value > 21;
+    % Returns true if hand value > 21
     bust = calculateHandValue(hand) > 21;
+end
+
+function [playerHand, nextCard, playerBust, playerStand] = playerTurn(shuffledDeck, nextCard, playerHand)
+    % Player chooses hit or stand
+    playerBust = false;
+    playerStand = false;
+
+    while ~playerBust && ~playerStand
+        disp('Your current hand:')
+        displayHand(playerHand);
+        fprintf('Current hand value: %d\n', calculateHandValue(playerHand));
+
+        choice = lower(input('Type "hit" to draw a card or "stand" to stop: ', 's'));
+
+        if strcmp(choice, 'hit')
+            [playerHand, nextCard] = hitCard(shuffledDeck, nextCard, playerHand);
+            disp('You drew:')
+            disp(playerHand(end))
+
+            if isBust(playerHand)
+                disp('Your updated hand:')
+                displayHand(playerHand);
+                fprintf('Hand value: %d\n', calculateHandValue(playerHand));
+                playerBust = true;
+            end
+
+        elseif strcmp(choice, 'stand')
+            playerStand = true;
+            disp('You chose to stand.')
+
+        else
+            disp('Invalid input. Please type "hit" or "stand".')
+        end
+
+        fprintf('\n');
+    end
+end
+
+function [dealerHand, nextCard] = dealerTurn(shuffledDeck, nextCard, dealerHand)
+    % Dealer must hit until hand value is at least 17
+    disp('Dealer reveals hand:')
+    displayHand(dealerHand);
+    fprintf('Dealer hand value: %d\n\n', calculateHandValue(dealerHand));
+
+    while calculateHandValue(dealerHand) < 17
+        disp('Dealer hits.')
+        [dealerHand, nextCard] = hitCard(shuffledDeck, nextCard, dealerHand);
+        disp('Dealer hand is now:')
+        displayHand(dealerHand);
+        fprintf('Dealer hand value: %d\n\n', calculateHandValue(dealerHand));
+    end
+
+    if isBust(dealerHand)
+        disp('Dealer busts!')
+    else
+        disp('Dealer stands.')
+    end
+    fprintf('\n');
+end
+
+function result = determineWinner(playerHand, dealerHand)
+    % Determine final winner
+    playerValue = calculateHandValue(playerHand);
+    dealerValue = calculateHandValue(dealerHand);
+
+    if playerValue > 21
+        result = 'You busted. Dealer wins.';
+    elseif dealerValue > 21
+        result = 'Dealer busted. You win!';
+    elseif playerValue > dealerValue
+        result = 'You win!';
+    elseif dealerValue > playerValue
+        result = 'Dealer wins.';
+    else
+        result = 'It is a tie.';
+    end
+end
+
+function displayHand(hand)
+    % Display hand as card values
+    disp(hand)
 end
